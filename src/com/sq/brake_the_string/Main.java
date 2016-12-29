4df651e036aef8ecbe0224e91bb49c2cd2de6641
VadimSquorpikkor <uniqdela@gmail.com>
2016-12-30 02:21:47 +0300

    insertSoft added

---
+++ src/com/sq/brake_the_string/Main.java
@@ -0,0 +1,10 @@
+package com.sq.brake_the_string;
+
+ // Created by SquorpikkoR on 23.12.2016.
+
+public class Main {
+    public static void main(String[] args) {
+        Braker br = new Braker();
+        br.brakeTheLine("vadim3");
+    }
+}
