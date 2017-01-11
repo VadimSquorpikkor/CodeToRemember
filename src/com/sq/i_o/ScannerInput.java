5fea0ca482cddeccaa9cb3f95642e61577f55e72
VadimSquorpikkor <uniqdela@gmail.com>
2017-01-11 16:36:35 +0300

    Fibo5

---
+++ src/com/sq/i_o/ScannerInput.java
@@ -0,0 +1,12 @@
+package com.sq.i_o; // Created by SquorpikkoR on 06.01.2017.
+
+import java.util.Scanner;
+
+public class ScannerInput {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+    }
+}
